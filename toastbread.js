0e4ab7cbd82873168cf2eb4126577b3f2af7dd66
Henrik Haugbølle <henrikhaugboelle@live.dk>
2011-07-25 17:13:36 +0200

    .

--- toastbread.js
+++ toastbread.js
@@ -3,15 +3,19 @@
 		SHUFFLE_ON: true, 
 		SHUFFLE_OFF: false,
 		
-		REPEAT_ONE: 'tb_repeat_one', 
-		REPEAT_ALL: 'tb_repeat_all', 
-		REPEAT_OFF: 'tb_repeat_off',
+		REPEAT_ONE: 2, 
+		REPEAT_ALL: 1, 
+		REPEAT_OFF: 0,
 		
 		onSetVolumeCallbacks: [],
+		onSetShuffleCallbacks: [],
+		onSetRepeatCallbacks: [],
 	
 		init: function() {	
 			var that = this;
 			console.log('Toastbread loaded');
+
+			
 			this.Queue.init();
 			
 			window.GS.player.tb_setVolume = window.GS.player.setVolume;
@@ -23,10 +27,31 @@
 				}
 				return window.GS.player.tb_setVolume(volume);
 			}
+
+			window.GS.player.tb_setShuffle = window.GS.player.setShuffle;
+			window.GS.player.setShuffle = function(shuffle) {
+				if (that.onSetShuffleCallbacks.length) {
+					_.forEach(that.onSetShuffleCallbacks, function (callback) {
+						callback.call(callback, shuffle);
+					});
+				}
+				return window.GS.player.tb_setShuffle(shuffle);
+			}
+
+			window.GS.player.tb_setRepeat = window.GS.player.setRepeat;
+			window.GS.player.setRepeat = function(repeat) {
+				if (that.onSetRepeatCallbacks.length) {
+					_.forEach(that.onSetRepeatCallbacks, function (callback) {
+						callback.call(callback, repeat);
+					});
+				}
+				
+				return window.GS.player.tb_setRepeat(repeat);
+			}
 		},
 		
 		play: function() {
-		
+			//if (GS.player && GS.player.queue && GS.player.queue.activeSong)GS.player.isPaused?GS.player.resumeSong(): GS.player.playSong(GS.player.queue.activeSong.queueSongID)
 		},
 		pause: function() {
 		
@@ -39,17 +64,23 @@
 		},
 
 		setShuffle: function(shuffle) {
-			
+			window.GS.player.tb_setShuffle(shuffle);
+		},
+		onSetShuffle: function(callback) {
+			this.onSetShuffleCallbacks.push(callback);
 		},
 		getShuffle: function() {
-		
+			return window.GS.player.getShuffle();
 		},
 		
 		setRepeat: function(repeat) {
-		
+			window.GS.player.tb_setRepeat(repeat);
+		},
+		onSetRepeat: function(callback) {
+			this.onSetRepeatCallbacks.push(callback);
 		},
 		getRepeat: function() {
-		
+			return window.GS.player.getRepeat();
 		},
 		
 		setVolume: function(volume) {
@@ -109,3 +140,5 @@
 	window.Toastbread = Toastbread;
 	Toastbread.init();
 })();
+
+// javascript:void((a = (b=document).createElement('script')).src='http://localhost:8888/toastbread/toastbread.js',b.body.appendChild(a))
